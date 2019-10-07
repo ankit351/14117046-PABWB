610216e0100cc17d6077ff55834463efa85a3669
Annisa Maharani <maharaniannisa80@gmail.com>
2019-10-07 11:45:06 +0700

    ADD minggu6

---
+++ pertemuan 6/minggu6/latihan1-1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title> Contoh Skript PHP</title>
+</head>
+<body>
+<?php
+$nama = "kim taehyung";
+echo "$nama berkata \"save me save me\" ";
+?>
+</body>
+</html>
---
+++ pertemuan 6/minggu6/latihan4.php
@@ -0,0 +1,16 @@
+<?php
+//MENAMPILKAN BILANGAN PRIMA 1-37
+ 
+for ($i = 1; $i<=37; $i++) {
+$a = 0;
+for ($j = 1; $j<=$i; $j++){
+if($i%$j==0){
+$a++;
+}
+}
+if($a==2){
+echo $i.'<br>';
+}
+}
+?>
+ 
